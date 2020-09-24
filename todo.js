c80cdb6d4ca8cb27f62af4da15bc16a119639bf1
Joy Christine <joychristin2@gmail.com>
2020-09-24 16:44:15 +0300

    add project & add task buttons functional

--- todo.js
+++ todo.js
@@ -1,16 +1,28 @@
 let addbtn = document.querySelector(".add-project");
 let addtask = document.querySelector(".add-task");
 let modal = document.querySelector(".modal");
+let modal2 = document.querySelector(".modal2");
 let closeButton = document.querySelector(".form-close");
+let closetask = document.querySelector(".task-close");
 
 
 addbtn.addEventListener("click", showModal);
+addtask.addEventListener("click", showModal2);
 closeButton.addEventListener("click", hideModal);
+closetask.addEventListener("click", hideModal2);
 
 function showModal() {
     modal.style.display = "block";
 }
 
+function showModal2() {
+    modal2.style.display = "block";
+}
+
 function hideModal() {
     modal.style.display = "none";
+}
+
+function hideModal2() {
+    modal2.style.display = "none";
 }
